6d559cbd7cd5d7f672e10fe50144cec471748885
Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
2024-09-28 03:41:20 +0900

    working commmit
    Signed-off-by: Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>

--- main.cpp
+++ main.cpp
@@ -1,71 +1,37 @@
-/*
- * Copyright (C) 2016 The Qt Company Ltd.
- *
- * SPDX-License-Identifier: Apache-2.0
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (C) 2021 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
-#include <QtCore/QDebug>
-#include <QtCore/QDir>
-#include <QtCore/QStandardPaths>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QQmlContext>
-#include <QtQml/qqml.h>
-#include <QtQuickControls2/QQuickStyle>
+#include <QCommandLineParser>
+#include <QDir>
 
-
-#include "playlistwithmetadata.h"
-
-QVariantList readMusicFile(const QString &path)
-{
-    QVariantList ret;
-    QDir dir(path);
-    for (const auto &entry : dir.entryList(QDir::Dirs | QDir::Files | QDir::NoDotAndDotDot, QDir::Name)) {
-        QFileInfo fileInfo(dir.absoluteFilePath(entry));
-        if (fileInfo.isDir()) {
-            ret.append(readMusicFile(fileInfo.absoluteFilePath()));
-        } else if (fileInfo.isFile()) {
-            ret.append(QUrl::fromLocalFile(fileInfo.absoluteFilePath()));
-        }
-    }
-    return ret;
-}
-
+using namespace Qt::Literals::StringLiterals;
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
     QGuiApplication app(argc, argv);
 
-    qmlRegisterType<PlaylistWithMetadata>("MediaPlayer", 1, 0, "PlaylistWithMetadata");
+    QCoreApplication::setApplicationName("MediaPlayer Example");
+    QCoreApplication::setOrganizationName("QtProject");
+    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QCoreApplication::translate("main", "Qt Quick MediaPlayer Example"));
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("url", QCoreApplication::translate("main", "The URL(s) to open."));
+    parser.process(app);
 
-    QVariantList mediaFiles;
-    for (const auto &music : QStandardPaths::standardLocations(QStandardPaths::MusicLocation)) {
-        mediaFiles.append(readMusicFile(music));
-    }
     QQmlApplicationEngine engine;
-    QQmlContext *context = engine.rootContext();
-    context->setContextProperty("mediaFiles", mediaFiles);
-    const QUrl url(QStringLiteral("qrc:/momiplay.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+    QObject::connect(&engine, &QQmlApplicationEngine::quit, &app, &QGuiApplication::quit);
+
+    if (!parser.positionalArguments().isEmpty()) {
+        QUrl source = QUrl::fromUserInput(parser.positionalArguments().at(0), QDir::currentPath());
+        engine.setInitialProperties({
+            { "source", source },
+        });
+    }
+    engine.loadFromModule("MediaPlayerModule", "Main");
 
     return app.exec();
 }
